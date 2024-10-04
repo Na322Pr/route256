d2f26419eda43b473dcb580bbd84ca2b14d70165
Alexander Marchenko <alex@MacBook-Air-Alexander.local>
2024-10-04 07:21:52 +0300

    Refactor tx_manager.go

--- internal/repository/postgres/tx_manager.go
+++ internal/repository/postgres/tx_manager.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"errors"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -13,11 +14,13 @@ type TxManager struct {
 	pool *pgxpool.Pool
 }
 
+type txFn = func(ctxTx context.Context) error
+
 func NewTxManager(pool *pgxpool.Pool) *TxManager {
 	return &TxManager{pool: pool}
 }
 
-func (m *TxManager) RunSerializable(ctx context.Context, fn func(ctxTx context.Context) error) error {
+func (m *TxManager) RunSerializable(ctx context.Context, fn txFn) error {
 	opts := pgx.TxOptions{
 		IsoLevel:   pgx.Serializable,
 		AccessMode: pgx.ReadOnly,
@@ -26,7 +29,7 @@ func (m *TxManager) RunSerializable(ctx context.Context, fn func(ctxTx context.C
 	return m.beginFunc(ctx, opts, fn)
 }
 
-func (m *TxManager) RunReadCommitted(ctx context.Context, fn func(ctxTx context.Context) error) error {
+func (m *TxManager) RunReadCommitted(ctx context.Context, fn txFn) error {
 	opts := pgx.TxOptions{
 		IsoLevel:   pgx.ReadCommitted,
 		AccessMode: pgx.ReadOnly,
@@ -35,14 +38,18 @@ func (m *TxManager) RunReadCommitted(ctx context.Context, fn func(ctxTx context.
 	return m.beginFunc(ctx, opts, fn)
 }
 
-func (m *TxManager) beginFunc(ctx context.Context, opts pgx.TxOptions, fn func(ctxTx context.Context) error) error {
+func (m *TxManager) beginFunc(ctx context.Context, opts pgx.TxOptions, fn txFn) error {
 	tx, err := m.pool.BeginTx(ctx, opts)
 	if err != nil {
 		return err
 	}
 
-	defer func() {
-		_ = tx.Rollback(ctx)
+	defer func() error {
+		err = errors.Join(err, tx.Rollback(ctx))
+		if err != nil {
+			return err
+		}
+		return nil
 	}()
 
 	ctx = context.WithValue(ctx, txManagerKey{}, tx)
